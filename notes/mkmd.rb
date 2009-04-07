07384aec6262d2a2548e52bbb22663b8116f0525
ashbb <ashbbb@gmail.com>
2009-04-08 01:49:51 +0900

    added the creating links function

--- notes/mkmd.rb
+++ notes/mkmd.rb
@@ -8,7 +8,11 @@ Dir.glob("*.md").each do |file|
   open(file, 'w') do |f|
     lines.each do |line|
       new_line = line
-      line.sub(/^# *(.*\.rb)/){new_line = read_src($1)}
+      #line.sub(/^# *(.*\.rb)/){new_line = read_src($1)}
+      line.sub(/^# *(.*\.rb)/) do
+        new_line = ["# [#{$1}](http://github.com/ashbb/dwemthys_array_study_note/blob/master/steps/#{$1})\n\n"]
+        new_line << read_src($1)
+      end
       f.puts new_line
     end
   end
